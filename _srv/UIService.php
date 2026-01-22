9768313c6c7f76cf7fff2946202333e6c46818f0
hatim.kamaal <hatim.kamaal@gmail.com>
2026-01-22 19:43:06 +0530

    fix#7

---
+++ _srv/UIService.php
@@ -0,0 +1,23 @@
+<?php
+
+class UIService {
+
+    public static function showOptions(array $options, $selected) {
+        if( count($options) == 0 ) {
+            return;
+        }
+
+        if( array_is_list($options) ) {
+            foreach( $options as $option ) {
+                $checked = $option === $selected ? 'selected' : '';              
+                echo "<option value='$option' $checked>$option</option>";
+            }                
+        } else {
+            foreach( $options as $value => $option ) {
+                $checked = $value === $selected ? 'selected' : '';              
+                echo "<option value='$value' $checked>$option</option>";
+            } 
+        }
+    }
+
+}
--- _view/add_member.php
+++ _view/add_member.php
@@ -10,45 +10,48 @@
         <div class="mb-3 row">
             <label for="staticEmail" class="col-sm-3 col-form-label">HOF ID</label>
             <div class="col-sm-9">
-                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="<?= $hof_id ?>">
+                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="<?= $dao->signin_hof_id ?>">
             </div>
         </div>
         <div class="mb-3 row">
             <label for="staticEmail" class="col-sm-3 col-form-label">ITS ID</label>
             <div class="col-sm-9">
                 <input type="text" title="8 digits ITS" required class="form-control" name="its_id"
-                    id="its_id" placeholder="ITS ID" pattern="^[0-9]{8}$">
+                    id="its_id" placeholder="ITS ID" pattern="^[0-9]{8}$" value="<?= $dao->its_id ?>">
             </div>
         </div>
         <div class="mb-3 row">
             <label for="full_name" class="col-sm-3 col-form-label">Full name</label>
             <div class="col-sm-9">
-                <input type="text" required class="form-control form-control-lg" id="full_name" name="full_name">
+                <input type="text" required class="form-control form-control-lg" id="full_name" name="full_name" value="<?= $dao->full_name ?>">
             </div>
         </div>
         <div class="mb-3 row">
             <label for="gender" class="col-sm-3 col-form-label">Gender</label>
             <div class="col-sm-9">
                 <select name="gender" class="form-control form-control-lg" required>
-                    <option value="">Select....</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
+                    <?= UIService::showOptions([''=>'Select..', 'Male'=>'Male', 'Female'=>'Female'], $dao->gender) ?>
+                    <!-- <option value=""  >Select....</option>
+                    <option value="Male" <?= $dao->gender == 'Male' ? 'selected' : '' ?>>Male</option>
+                    <option value="Female" <?= $dao->gender == 'Female' ? 'selected' : '' ?>>Female</option> -->
                 </select>
             </div>
         </div>
         <div class="mb-3 row">
             <label for="age" class="col-sm-3 col-form-label">Age</label>
             <div class="col-sm-9">
-                <input type="text" pattern="^[0-9]{1,2}$" required class="form-control" id="age" name="age">
+                <input type="text" pattern="^[0-9]{1,2}$" required class="form-control" id="age" name="age"  value="<?= $dao->age ?>">
             </div>
         </div>
         <div class="mb-3 row">
             <label for="misaq" class="col-sm-3 col-form-label">Misaq</label>
             <div class="col-sm-9">
                 <select name="misaq" class="form-control form-control-lg" required>
-                    <option value="">Select....</option>
+                    <?= UIService::showOptions([''=>'Select..', 'Done'=>'Done', 'Not Done'=>'Not Done'], $dao->misaq) ?>
+
+                    <!-- <option value="">Select....</option>
                     <option value="Done">Done</option>
-                    <option value="Not Done">Not Done</option>
+                    <option value="Not Done">Not Done</option> -->
                 </select>
             </div>
         </div>
