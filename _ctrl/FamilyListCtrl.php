0f01004430b055d79d67e34a42f90322198752f1
hatim.kamaal <hatim.kamaal@gmail.com>
2026-01-23 16:26:43 +0530

    objecft ref changed

--- _ctrl/FamilyListCtrl.php
+++ _ctrl/FamilyListCtrl.php
@@ -10,7 +10,7 @@ class FamilyListCtrl extends Ctrl {
 
         $takhRecord = $dbctrl->getTakhmeenRecordFor($hof_id);
         $dao->takhRecord = $takhRecord;
-        $dao->pirsa_selected = ( $dao->takhRecord && $dao->takhRecord->pirsa_count > 0 ) ? ' checked ' : '';
+        $dao->pirsa_selected = ( isset($takhRecord) && $takhRecord->pirsa_count > 0 ) ? ' checked ' : '';
 
         $this->render('family_list' , $dao);        
     }
