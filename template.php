d3a26c15294fb1f169f1203c76e413f14f9a4f57
hatim.kamaal <hatim.kamaal@gmail.com>
2026-01-23 17:19:08 +0530

    SS

--- template.php
+++ template.php
@@ -36,7 +36,7 @@ $home = $dao->home_uri;
             <div class="container-fluid">
                 <div class="row align-items-center">
                     <div class="col-4">
-                        <a href="<?=$home?>"><img class="img-fluid mx-auto d-block" src="<?= $home ?>/assets/img/ramzan_logo.png"
+                        <a href="<?=$home?>"><img class="img-fluid mx-auto d-block" src="<?= $home ?>/assets/img/ramazan_logo.png"
                             alt="Hatimi Hills" height="53" width="53"/></a>
                     </div>
         <?php if($isSecured) {?>
