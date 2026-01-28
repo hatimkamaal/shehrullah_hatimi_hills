23eb320620539907ff9c9da41c49328bbcb60f57
hatim.kamaal <hatim.kamaal@gmail.com>
2026-01-28 19:35:54 +0530

    Login

--- _ctrl/LoginCtrl.php
+++ _ctrl/LoginCtrl.php
@@ -3,58 +3,113 @@
 class LoginCtrl extends Ctrl
 {
 
+// public function get(Dao $dao)
+//   {
+
+//     require_once('vendor/Google/autoload.php');
+
+//     $client = new Google_Client();
+//     $client->setClientId(GOOGLE_CLIENT_ID);
+//     $client->setClientSecret(GOOGLE_CLIENT_SECRET);
+//     $client->setRedirectUri(GOOGLE_REDIRECT_URL);
+//     $client->addScope("email");
+//     $client->addScope("profile");
+
+//     if (isset($_GET['code'])) {
+//       $client->authenticate($_GET['code']);
+//       $_SESSION['access_token'] = $client->getAccessToken();
+//       header('Location: ' . filter_var(GOOGLE_REDIRECT_URL, FILTER_SANITIZE_URL));
+//       exit;
+//     }
+
+//     $service = new Google_Service_Oauth2($client);
+
+//     if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+//       $client->setAccessToken($_SESSION['access_token']);
+
+//       $user = $service->userinfo->get(); //get user info 
+//       $email = $user->email;
+
+//       $db = new DBService();
+//       // $db->setUserSession(Dao::construct(['email'=>$email, 'hof_id'=>-1,'state'=>'NEW']));
+
+//       $loginData = $db->getUserLoginData($email);
+//       if( is_null($loginData) ) {
+//           $this->setTransitMessage('Oops! Can not locate your profile ('.$email.'). <a href="https://wa.me/8390403052" target="_blank">Click to Chat with us</a>.');
+//       } else {
+//         if( in_array( $loginData->sector, [7,13])  ) {
+
+//           $user_roles = $db->get_user_roles($loginData->id);
+//           $loginData->roles = explode(',' , $user_roles);
+
+//           $loginData->state = 'LINKED';
+//           $db->setUserSession($loginData);
+//           $uri = $dao->home_uri . '/' . LANDING_PAGE;
+//           header('Location: ' . $uri);
+//         } else {
+//           $this->setTransitMessage('Oops! Not found in Hatimi Hills Sector ('.$email.'). <a href="https://wa.me/8390403052" target="_blank">Click to Chat with us</a>.');
+//         }
+//       }
+
+//       $dao->authUrl = $dao->home_uri . '/login';
+//     } else {
+//       $dao->authUrl = $client->createAuthUrl();
+//     }
+
+//       $this->render('login', $dao);
+//   }
   public function get(Dao $dao)
   {
 
-    require_once('vendor/Google/autoload.php');
+    // require_once('vendor/Google/autoload.php');
 
-    $client = new Google_Client();
-    $client->setClientId(GOOGLE_CLIENT_ID);
-    $client->setClientSecret(GOOGLE_CLIENT_SECRET);
-    $client->setRedirectUri(GOOGLE_REDIRECT_URL);
-    $client->addScope("email");
-    $client->addScope("profile");
+    // $client = new Google_Client();
+    // $client->setClientId(GOOGLE_CLIENT_ID);
+    // $client->setClientSecret(GOOGLE_CLIENT_SECRET);
+    // $client->setRedirectUri(GOOGLE_REDIRECT_URL);
+    // $client->addScope("email");
+    // $client->addScope("profile");
 
-    if (isset($_GET['code'])) {
-      $client->authenticate($_GET['code']);
-      $_SESSION['access_token'] = $client->getAccessToken();
-      header('Location: ' . filter_var(GOOGLE_REDIRECT_URL, FILTER_SANITIZE_URL));
-      exit;
-    }
+    // if (isset($_GET['code'])) {
+    //   $client->authenticate($_GET['code']);
+    //   $_SESSION['access_token'] = $client->getAccessToken();
+    //   header('Location: ' . filter_var(GOOGLE_REDIRECT_URL, FILTER_SANITIZE_URL));
+    //   exit;
+    // }
 
-    $service = new Google_Service_Oauth2($client);
+    // $service = new Google_Service_Oauth2($client);
 
-    if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-      $client->setAccessToken($_SESSION['access_token']);
+    // if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+    //   $client->setAccessToken($_SESSION['access_token']);
 
-      $user = $service->userinfo->get(); //get user info 
-      $email = $user->email;
+    //   $user = $service->userinfo->get(); //get user info 
+    //   $email = $user->email;
 
-      $db = new DBService();
-      // $db->setUserSession(Dao::construct(['email'=>$email, 'hof_id'=>-1,'state'=>'NEW']));
+    //   $db = new DBService();
+    //   // $db->setUserSession(Dao::construct(['email'=>$email, 'hof_id'=>-1,'state'=>'NEW']));
 
-      $loginData = $db->getUserLoginData($email);
-      if( is_null($loginData) ) {
-          $this->setTransitMessage('Oops! Can not locate your profile ('.$email.'). <a href="https://wa.me/8390403052" target="_blank">Click to Chat with us</a>.');
-      } else {
-        if( in_array( $loginData->sector, [7,13])  ) {
+    //   $loginData = $db->getUserLoginData($email);
+    //   if( is_null($loginData) ) {
+    //       $this->setTransitMessage('Oops! Can not locate your profile ('.$email.'). <a href="https://wa.me/8390403052" target="_blank">Click to Chat with us</a>.');
+    //   } else {
+    //     if( in_array( $loginData->sector, [7,13])  ) {
 
-          $user_roles = $db->get_user_roles($loginData->id);
-          $loginData->roles = explode(',' , $user_roles);
+    //       $user_roles = $db->get_user_roles($loginData->id);
+    //       $loginData->roles = explode(',' , $user_roles);
 
-          $loginData->state = 'LINKED';
-          $db->setUserSession($loginData);
-          $uri = $dao->home_uri . '/' . LANDING_PAGE;
-          header('Location: ' . $uri);
-        } else {
-          $this->setTransitMessage('Oops! Not found in Hatimi Hills Sector ('.$email.'). <a href="https://wa.me/8390403052" target="_blank">Click to Chat with us</a>.');
-        }
-      }
+    //       $loginData->state = 'LINKED';
+    //       $db->setUserSession($loginData);
+    //       $uri = $dao->home_uri . '/' . LANDING_PAGE;
+    //       header('Location: ' . $uri);
+    //     } else {
+    //       $this->setTransitMessage('Oops! Not found in Hatimi Hills Sector ('.$email.'). <a href="https://wa.me/8390403052" target="_blank">Click to Chat with us</a>.');
+    //     }
+    //   }
 
-      $dao->authUrl = $dao->home_uri . '/login';
-    } else {
-      $dao->authUrl = $client->createAuthUrl();
-    }
+    //   $dao->authUrl = $dao->home_uri . '/login';
+    // } else {
+    //   $dao->authUrl = $client->createAuthUrl();
+    // }
 
       $this->render('login', $dao);
   }
