ed4eecc798f0cbd44755c2f6ae5db1500cc2d554
hatim.kamaal <hatim.kamaal@gmail.com>
2026-01-29 00:07:07 +0530

    server is down

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+echo 'Site is under maintenance. Please check back later.';
+exit();
+
 
 // error_reporting(E_ALL ^ E_NOTICE);  
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 DEFINE('LANDING_PAGE', 'Home');
