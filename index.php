484bacfdfcfa5399055c9f8ed061840c1bd03979
hatim.kamaal <hatim.kamaal@gmail.com>
2026-01-29 00:34:50 +0530

    Moved

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-echo 'Site is under maintenance. Please check back later.';
-exit();
+header("Location: https://hatimihills.free.nf");
+exit(); // Important: stop further script execution
 
+// // error_reporting(E_ALL ^ E_NOTICE);  
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-// error_reporting(E_ALL ^ E_NOTICE);  
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// DEFINE('LANDING_PAGE', 'Home');
 
-DEFINE('LANDING_PAGE', 'Home');
-
-include_once 'eco_sys.php';
-$ecosys = new Eco_sys();
-$ecosys->bootstrap();
+// include_once 'eco_sys.php';
+// $ecosys = new Eco_sys();
+// $ecosys->bootstrap();
